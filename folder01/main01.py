4299393f3c00dc33cf20de05a91bd58565663d73
Prettimallika <prettydeori242@gmail.com>
2020-09-06 21:27:03 +0530

    made changes by pretty

--- folder01/main01.py
+++ folder01/main01.py
@@ -1,6 +1,9 @@
 def display():
 	print("Hello from folder01 by Sanjoy pator")
-
+        
+#function pretty = folder04
+def pretty():
+   print("Hello Pretty - folder01")
 
 display()
     
--- folder02/main02.py
+++ folder02/main02.py
@@ -1,6 +1,11 @@
 def display():
 	print("Hello from folder02 by Sanjoy pator")
 
+# function pretty 
+def pretty():
+      print("Hello pretty - folder02")
+    
+
 
 display()
     
