0788672e9d320c425377d1bcdf71c30f86bfd10c
Jeherul Islam <j.islam5225@gmail.com>
2020-09-06 21:21:59 +0530

    first git change by jeherul

--- folder01/main01.py
+++ folder01/main01.py
@@ -2,5 +2,12 @@ def display():
 	print("Hello from folder01 by Sanjoy pator")
 
 
+#finction Jeherul - folder01
+def JI():
+	print("Hrllo from JI - folder01")
+
 display()
+
+#function call - JI
+JI()
     
--- folder02/main02.py
+++ folder02/main02.py
@@ -1,6 +1,13 @@
 def display():
 	print("Hello from folder02 by Sanjoy pator")
 
+#finction Jeherul - folder02
+def JI():
+	print("Hrllo from JI - folder02")
+
 
 display()
+
+#function call - JI
+JI()
     
