327b7910bbd47751273ef23836e3bb98d64e0917
unknown <dikshita.sarmah@rediffmail.com>
2020-09-06 21:35:36 +0530

    First git change by Dikshita

--- folder01/main01.py
+++ folder01/main01.py
@@ -1,6 +1,12 @@
 def display():
 	print("Hello from folder01 by Sanjoy pator")
 
+#Dikshita Sarmah
+def dikshita():
+	print("Hello from folder01 by Dikshita Sarmah")
+
 
 display()
-    
+
+#Dikshita Sarmah
+dikshita()
--- folder02/main02.py
+++ folder02/main02.py
@@ -1,6 +1,11 @@
 def display():
 	print("Hello from folder02 by Sanjoy pator")
 
+#Dikshita Sarmah
+def dikshita():
+	print("Hello from folder02 by Dikshita Sarmah")
 
 display()
-    
+
+#Dikshita Sarmah
+dikshita()
