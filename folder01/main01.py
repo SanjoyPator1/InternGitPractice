0038cdb82bbe8661b9098b8d9b0d1170fd6d862b
Rhythm kumar Das <sonurokings@gmail.com>
2020-09-06 21:13:48 +0530

    these are my changes by Rhythm

--- folder01/main01.py
+++ folder01/main01.py
@@ -1,6 +1,10 @@
 def display():
 	print("Hello from folder01 by Sanjoy pator")
+    #edited by RHYTHM
+ def Rhythm()
+     print("RHythm -folder1")
 
 
 display()
+Rhythm()
     
--- folder02/main02.py
+++ folder02/main02.py
@@ -1,6 +1,10 @@
 def display():
 	print("Hello from folder02 by Sanjoy pator")
+    #edited by rhythm
+ def Rhythm():
+     print("hello - from2")
 
 
 display()
+Rhythm()
     
