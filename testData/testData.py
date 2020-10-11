d7697129407a60177fa02b2fced3aa7833554e96
SanjoyPator <sanjoypator1@gmail.com>
2020-10-11 21:31:42 +0530

    try except added to know the exact position of error

--- testData/testData.py
+++ testData/testData.py
@@ -19,8 +19,30 @@ ignore_words = ['?', '!']
 data_file = open('intents.json', encoding="utf8").read()
 intents = json.loads(data_file)
 
+def count():
+    global counter
+    
+
+counter = 0
+#a = True
+
+'''try:
+    for i in  range(8):
+        counter=counter+1
+        if (counter==3):
+            raise Exception("manual exception and counter is - ")
+        if (counter==5):
+            a=False
+except Exception as error:
+    print("The counter now is - broken while -jcnsjdc the counter is - ",counter)'''
+
+#print("The counter now is -jcnsjdc ", counter)
+
+# try except for debugging - lol
+
 try:
     for intent in intents['intents']:
+        
 
         try:
             for pattern in intent['patterns']:
@@ -35,13 +57,17 @@ try:
                     # adding classes to our class list
                     if intent['tag'] not in classes:
                         classes.append(intent['tag'])
+                        
+                    counter=counter+1
+                    print("counter inside main - is - ", counter)
+                    
                 except:
-                    print("An exception occurred inside for loop main code")
+                    print("An exception occurred inside for loop main code in counter - ",counter)
 
-        except:
-            print("An exception occurred in 2nd for loop patterns")
+        except Exception as error:
+            print("An exception occurred in 2nd for loop patterns in counter = ",counter)
 except:
-  print("An exception occurred in 1st for loop tag")
+  print("An exception occurred in 1st for loop tag part -", counter)
 
 
 
@@ -61,29 +87,5 @@ print (len(words), "unique lemmatized words", words)
 pickle.dump(words,open('words.pkl','wb'))
 pickle.dump(classes,open('classes.pkl','wb'))
 
-# initializing training data
-training = []
-output_empty = [0] * len(classes)
-for doc in documents:
-    # initializing bag of words
-    bag = []
-    # list of tokenized words for the pattern
-    pattern_words = doc[0]
-    # lemmatize each word - create base word, in attempt to represent related words
-    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # create our bag of words array with 1, if word match found in current pattern
-    for w in words:
-        bag.append(1) if w in pattern_words else bag.append(0)
 
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
-    output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
-
-    training.append([bag, output_row])
-# shuffle our features and turn into np.array
-random.shuffle(training)
-training = np.array(training)
-# create train and test lists. X - patterns, Y - intents
-train_x = list(training[:,0])
-train_y = list(training[:,1])
 print("Training data created")
